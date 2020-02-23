97ef10bfaba77ddd076aaed68c4cef22125679e9
mzima <martin.zimmermann@gmx.com>
2020-02-23 16:31:41 +0100

    Added Debug functionality

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ COPY image.jpg /usr/share/nginx/html
 RUN chmod +x /start.sh
 
 ENV who="stranger"
+ENV debug="false"
 
 EXPOSE 80
 ENTRYPOINT ["./start.sh"]
--- start.sh
+++ start.sh
@@ -5,5 +5,13 @@ sys_hostname="`uname -n`"
 echo "Start up ..."
 sed -i "s/FUBAR/$who/g" /usr/share/nginx/html/index.html
 sed -i "s/SYS_HOSTNAME/$sys_hostname/g" /usr/share/nginx/html/index.html
-nginx -g 'daemon off;'
 
+if [ $debug = "true" ]
+then
+    echo "<html><h1>Debug:</h1>" > /usr/share/nginx/html/debug.html
+    env >> /usr/share/nginx/html/debug.html
+    sed -i ':a;N;$!ba;s/\n/<br>\n/g' /usr/share/nginx/html/debug.html
+    echo "</html>" >> /usr/share/nginx/html/debug.html
+fi
+
+nginx -g 'daemon off;'
